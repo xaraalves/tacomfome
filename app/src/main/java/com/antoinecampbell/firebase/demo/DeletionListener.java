a994fb144854fbf40df8b083f3f61f68f05f9a8e
Gabriel Alves <bt.gabriel.alves@gmail.com>
2017-10-14 22:29:37 +0000

    limpeza para instaurar novo projeto

--- app/src/main/java/com/antoinecampbell/firebase/demo/DeletionListener.java
+++
@@ -1,6 +0,0 @@
-package com.antoinecampbell.firebase.demo;
-
-public interface DeletionListener
-{
-    void itemRemoved(int position);
-}
--- tacomfome-alternativo/Tacomfome2/app/src/main/java/ufscar/tacomfome/tacomfome/interfaces/DeletionListener.java
+++
@@ -1,6 +0,0 @@
-package ufscar.tacomfome.tacomfome.interfaces;
-
-public interface DeletionListener
-{
-    void itemRemoved(int position);
-}
